c1802c4e44e9c89cacfd630102843d963b59b0c7
Lareciyo <lmcneal@perseverenow.org>
2025-08-21 07:17:22 -0500

    second comment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import React from 'react'
+import Imager from './components/Imager'
 
 function App() {
   return (
     <div>
       <h1>React Imager App</h1>
+      <Imager />
     </div>
   );
 }
